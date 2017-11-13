0f34fb6320c27087f645bdc5cad41b8317574b4c
Alejandro Cuiñas <alejandro.cuinas@redbility.com>
2017-11-13 13:14:33 +0100

    Creación de los primeros componentes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,43 @@
 import Vue from 'vue'; // Importamos Vue
 import './style.scss'; // Importamos Sass
+import genres from './util/genres'; // Importamos el mock de generos
 
 new Vue({ // Instanciamos la app
     el: '#app',
-    data: {
-        msg: 'Hello Lemmy!'
+    components: {
+        'movie-list': {
+            template: `<div id="movie-list">
+                        <div class="movie" v-for="movie in movies">
+                            {{ movie.title }}
+                        </div>
+                    </div>`,
+            data: function() {
+                return {
+                    movies: [
+                        { title: 'Pulp Fiction'},
+                        { title: 'Home Alone'},
+                        { title: 'Austin Powers'},
+                    ]
+                }
+            }
+        },
+        'movie-filter': {
+            template: `<div id="movie-filter">
+                    <h2>Filter results</h2>
+                    <div class="filter-group">
+                        <check-filter v-for="(genre, index) in genres">{{ genre(index) }}</check-filter>
+                    </div>
+                </div>`,
+            data: function() {
+                return {
+                    genres
+                }
+            },
+            components: {
+                'check-filter': {
+                    template: `<div>Filter</div>`
+                }
+            }
+        }
     }
 })
