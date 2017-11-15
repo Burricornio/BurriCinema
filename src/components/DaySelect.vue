45f5901243d9fb076a658f7ea2c44f19fbde6f53
Alejandro Cuiñas <alejandro.cuinas@redbility.com>
2017-11-15 17:59:30 +0100

    Avanzamos en la creación del componente ‘DaySelect’

--- src/components/DaySelect.vue
+++ src/components/DaySelect.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="day-select">
-      Hellooooooo
+      <ul class="days">
+          <li class="day" v-for="(day, index) in days" :key="index">{{ formatDay(day) }}</li>
+      </ul>
   </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                days: [0,1,2,3,4,5,6].map( num => this.$moment().add(num, 'days'))
+            }
+        },
+        methods: {
+            formatDay(raw) {
+                if(raw.isSame(this.$moment(), 'day')) {
+                    return 'Today';
+                } else {
+                    return raw.format('ddd DD/MM');
+                }      
+            }
+        }
+    }
+</script>
