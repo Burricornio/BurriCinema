df1625d85458b0c6ce21a519b1079dbe4015a4ea
Alejandro Cuiñas <alejandro.cuinas@redbility.com>
2017-11-15 17:02:52 +0100

    Mostrando los datos de una película en el componente ‘Detail’

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>Hello World</div>
+    <div id="detal" v-if="movie">
+        <movie-item v-bind:movie="movie.movie" v-bind:sessions="[]"></movie-item>
+    </div>
 </template>
 
 <script>
+    import MovieItem from './MovieItem.vue';
+
     export default {
+        props: [ 'movies' ],
+        computed: {
+            movie() {
+                let movie =  this.movies.find( movie => movie.id === this.$route.params.id);
+                return movie ? movie : null;
+            
+            }
+        },
         created() {
             console.log(this.$route.params.id);
+        },
+        components: {
+            MovieItem
         }
     }
 </script>
