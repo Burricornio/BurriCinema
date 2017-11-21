fec3a57f3d71b47bf75725c50bd80731ae3e2b57
Alejandro Cuiñas <alejandro.cuinas@redbility.com>
2017-11-21 11:10:12 +0100

    Seguimos con el componente para seleccionar los días

--- src/components/DaySelect.vue
+++ src/components/DaySelect.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="day-select">
       <ul class="days">
-          <li class="day" v-for="(day, index) in days" :key="index">{{ formatDay(day) }}</li>
+          <li v-bind:class="{ day: true, active: isActive(day) }" v-for="(day, index) in days" :key="index" v-on:click="selected =  day">{{ formatDay(day) }}</li>
       </ul>
   </div>
 </template>
@@ -9,6 +9,7 @@
     export default {
         data() {
             return {
+                selected: this.$moment(),
                 days: [0,1,2,3,4,5,6].map( num => this.$moment().add(num, 'days'))
             }
         },
@@ -19,6 +20,9 @@
                 } else {
                     return raw.format('ddd DD/MM');
                 }      
+            },
+            isActive(day) {
+                return day.isSame(this.selected, 'day');
             }
         }
     }
