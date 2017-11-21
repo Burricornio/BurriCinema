f3713f97d02f0baf30f3cbd9e83dad514f3943f4
Alejandro Cuiñas <alejandro.cuinas@redbility.com>
2017-11-21 13:35:36 +0100

    Retoques componente tooltip

--- src/main.js
+++ src/main.js
@@ -58,19 +58,19 @@ import { addClass, removeClass } from './util/helpers';
 
 let mouseOutHandler = function(event) {
 	let span = event.target.parentNode.getElementsByTagName('span')[0];
-	addClass(span, 'tooltip-show');
+	removeClass(span, 'tooltip-show');
 }
 
 let mouseOverHandler = function(event) {
 	let span = event.target.parentNode.getElementsByTagName('span')[0];
-	removeClass(span, 'tooltip-show');
+	addClass(span, 'tooltip-show');
 	
 }
 
 Vue.directive('tooltip', {
 	bind(el, bindings) {
 		let span = document.createElement('span');
-		let text = document.createTextNode('Seats available: 200');
+		let text = document.createTextNode(`Seats available: ${bindings.value.seats}`);
 		span.appendChild(text);
 		addClass(span, 'tooltip');
 		el.appendChild(span);
